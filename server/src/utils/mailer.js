7a63b4c2b5bc7252c8e7c69efeded63be3d8969d
Haisers <vhynnc57044@gmail.com>
2026-09-21 22:27:15 +0800

    Debug mailer loading

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -1,9 +1,12 @@
 const nodemailer = require('nodemailer');
 
+console.log('[MAILER] mailer.js loaded');
+
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    secure: Number(process.env.SMTP_PORT) === 465,
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
